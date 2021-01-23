6819dbbb197ca94b356b8a8626d23a7bbe34fb76
”sovsianykov” <swebproger@gmail.com>
2021-01-23 20:32:39 +0200

    variant

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,12 +5,13 @@ function Home() {
     return (
 
         <div className="Home container-fluid ">
-            {/*<h1>Valentin Kornyenko</h1>*/}
-            {/*<h2>bass player & composer & teacher</h2>*/}
              <Row>
-                 <Col md='3'/>
+                 <Col md='2'/>
                  <Col md='6' className='hero'/>
-                 <Col md='3'/>
+                 <Col md='4' className='tittle'>
+                     <h1>Valentin Kornyenko</h1>
+                     <h2>bass player  composer  teacher</h2>
+                 </Col>
              </Row>
         </div>
     );
