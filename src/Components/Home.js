a77c450f86d1c529d73bd2974f1c4c0d184920c6
”sovsianykov” <swebproger@gmail.com>
2021-01-23 18:08:48 +0200

    first

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.scss';
+function Home() {
+    return (
+        <div className="Home">
+            <h1>Valentin Kornyenko</h1>
+            <h2>bass player & composer & teacher</h2>
+        </div>
+    );
+}
+
+export default Home;
